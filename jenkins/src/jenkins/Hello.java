59392d813b98a0d598dee9623494dc287deba0ec
Reji <veerendramani@gmail.com>
2018-09-09 15:02:16 +0530

    commit3

--- jenkins/src/jenkins/Hello.java
+++ jenkins/src/jenkins/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hello java");
-
+		System.out.println("hello java World");
+		System.out.println("hello java World2");
 	}
 
 }
